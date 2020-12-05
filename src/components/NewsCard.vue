d555ff615a75ff57f2ddb6ef1a3e636d59a60a21
Luis Antonio Parrado <luisprmat@gmail.com>
2020-12-05 16:36:38 -0500

    Agrega la API de Noticias con un select

---
+++ src/components/NewsCard.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="col mb-2">
+  <div class="card shadow mb-3">
+    <div class="row no-gutters">
+      <div class="col-lg-5">
+        <img :src="item.urlToImage" class="card-img" :alt="item.title" v-if="item.urlToImage">
+        <div class="card-body">
+          <h4 class="card-title">{{ item.title }}</h4>
+        </div>
+      </div>
+      <div class="col-lg-7">
+        <div class="card-body">
+          <p class="card-text">{{ item.content }}</p>
+          <hr>
+          <p class="card-text"><a :href="item.url" class="btn btn-link">Leer noticia completa >></a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ["item"]
+}
+</script>
