dad28faad8105987eb2e99549968cfea843c7b92
restrenal <restrenalrm@gmail.com>
2020-12-04 19:13:55 -0500

    Se agregan componentes API y Sección Servicios

---
+++ src/components/MisServicios.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- Inicio sección MUSIC -->
+            <div class="container">
+                <h2 class="text-center mb-3">Escucha tu música preferida</h2>
+                <div class="row">
+                    <template>
+
+                        <div class="col-sm-4" v-for="item in info" :key="item.id">
+                            <div class="card">
+                                <img :src="'https://www.dailymotion.com/thumbnail/video/' + item.id" alt=""
+                                    class="car-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{item.title}}</h5>
+                                    <a v-bind:href="'https://www.dailymotion.com/video/' + item.id"
+                                        class="btn btn-primary">Reproducir</a>
+
+                                </div>
+                            </div>
+                        </div>
+                    </template>
+
+                </div>
+
+            </div>
+
+</template>
+
+
+
+
+<script>
+import axios from'axios'
+export default {
+    data(){
+        return{
+            info: []
+        }
+    },
+
+    mounted(){
+            axios.get("https://api.dailymotion.com/videos?channel=music&limit=3")
+                            .then(respuesta => this.info = respuesta.data.list)
+    }
+}
+</script>
---
+++ src/components/ServiciosTele.vue
@@ -0,0 +1,84 @@
+<template>
+                <!-- Inicio sección SERVICIOS TELECOMUNICACIONES -->
+
+       <div class="container">
+            <div class="mb-3">
+                <h2 class="text-center mb-3">Servicios en telecomunicaciones</h2>
+                <div class="row">
+
+                    <!-- Servicio de telefonía -->
+                    <div class="col-md-4">
+                        <div class="card text-left shadow-sm mb-2">
+                            <a href="https://www.claro.com.co/personas/servicios/servicios-moviles/">
+                                    <img class="card-img-top" alt="Telefonía móvil" src="/images/mobil.png">
+                                <!-- <img :src="require('./assets/mobil.png')" alt=""> -->
+                                <!-- <v-img src="@/assets/mobil.png"></v-img> -->
+
+
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">Telefonía móvil</h4>
+                                <p class="card-text">
+                                    La Telefonía Móvil Celular es un servicio público de telecomunicaciones, cuyo
+                                    objetivo principal es facilitar la comunicación telefónica de los abonados sin
+                                    imponer restricciones en cuanto a ubicación y desplazamiento de éstos.El servicio es
+                                    económico y se instala de manera inmediata.
+                                </p>
+                                <a href="https://www.claro.com.co/personas/servicios/servicios-moviles/"
+                                    class="btn btn-primary">Contrátalo</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Servicio de internet hogar-->
+                    <div class="col-md-4">
+                        <div class="card text-left shadow-sm mb-2">
+                            <a href="https://www.movistar.co/planes-de-internet/">
+                                     <img class="card-img-top" src="/images/internet.png" alt="Internet Hogar">
+                                <!-- <img :src="require('./assets/internet.png')" alt=""> -->
+                                <!-- <v-img src="@/assets/mobil.png"></v-img> -->
+
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">Internet hogar</h4>
+                                <p class="card-text">
+                                    El servicio de internet se establece creando una conexión entre sus oficinas
+                                    centrales y la casa. Esta conexión puede realizarse utilizando varios medios y
+                                    tecnologías. Las tecnologías y medios de acceso a internet más utilizadas en nuestro
+                                    país son: ADSL, DOCSIS, GPON.
+                                </p>
+                                <a href="https://www.movistar.co/planes-de-internet"
+                                    class="btn btn-primary">Contrátalo</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Servicio de televisión -->
+                    <div class="col-md-4">
+                        <div class="card text-left shadow-sm">
+                            <a href="https://www.tigo.com.co/servicios/television">
+                                     <img class="card-img-top" src="/images/tv.png" alt="Televisión">
+                                <!-- <img :src="require('./assets/tv.png')" alt=""> -->
+                                <!-- <v-img src="@/assets/mobil.png"></v-img> -->
+
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">Televisión</h4>
+                                <p class="card-text">
+                                    La televisión por cable consiste en un servicio de TV en el cual la señal llega al
+                                    abonado a través de un sistema de fibra óptica o bien vía coaxial. En esta nueva
+                                    oferta destacan las smart TV y también otras formas de disfrutar de los contenidos.
+                                    Es un servicio de conexión instantánea.
+                                </p>
+                                <a href="https://www.tigo.com.co/servicios/television"
+                                    class="btn btn-primary">Contrátalo</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <!-- Fin seccion de servicios -->
+ </div>
+</template>
