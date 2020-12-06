6846a4d83decf2ca4dd2624c59558e7b6318d8f5
Luis Antonio Parrado <luisprmat@gmail.com>
2020-12-06 13:56:34 -0500

    Deploy page

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mintic-group-107/semana-2-107.git release:gh-pages
+git push -f https://github.com/mintic-group-107/semana-2-107.git gh-pages:gh-pages
 
 cd -
