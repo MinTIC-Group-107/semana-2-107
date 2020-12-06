06d2eb08c9fafa7344a892362dce705adaa4b59e
Luis Antonio Parrado <luisprmat@gmail.com>
2020-12-05 21:10:35 -0500

    Ajustes al diseño de la card

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -2,13 +2,13 @@
 <div class="col mb-2">
   <div class="card shadow mb-3">
     <div class="row no-gutters">
-      <div class="col-lg-5">
+      <div class="col-lg-7">
         <img :src="item.urlToImage" class="card-img" :alt="item.title" v-if="item.urlToImage">
         <div class="card-body">
-          <h4 class="card-title">{{ item.title }}</h4>
+          <h5 class="card-title">{{ item.title }}</h5>
         </div>
       </div>
-      <div class="col-lg-7">
+      <div class="col-lg-5">
         <div class="card-body">
           <p class="card-text">{{ item.content }}</p>
           <hr>
